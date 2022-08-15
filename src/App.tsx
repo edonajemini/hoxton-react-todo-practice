2447ea407960c7bed6ff2e0e8b69827018e8ffcd
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-15 16:07:39 +0200

    The app looks fine

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ function App() {
 
   return (
     <div className="App">
+      <h1>To do APP</h1>
+      <ul className='first-list'>
+        <li className='done-list'>Done ✔️
+          <li className='done-list-smaller'>Learn HTML</li>
+          <li className='done-list-smaller'>Learn CSS</li>
+          <li className='done-list-smaller'>Learn JS</li>
+          <li className='done-list-smaller'>Learn TypeScript</li>
+          <li className='done-list-smaller'>Learn React</li>
+        </li>
+
+        <li className='notdone-list'>Not done yet!
+            <li className='done-list-smaller'>Learn servers</li>
+            <li className='done-list-smaller'>Learn Node </li>
+            <li className='done-list-smaller'>Learn Java</li>
+        </li>
+      </ul>
     </div>
   )
 }
